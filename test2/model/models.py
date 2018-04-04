1bcfb5f259f229c2a86d318a794580d80f53a137
2siwon <lee89640245@gmail.com>
2018-04-04 20:03:13 +0900

    model app 추가

--- test2/model/models.py
+++ test2/model/models.py
@@ -12,3 +12,25 @@ class Person(models.Model):
         max_length=1, choices=SHIRT_SIZES)
 
 
+class Fruit(models.Model):
+    name = models.CharField(
+        max_length=100,
+        primary_key=True,
+    )
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        Manufacturer, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.manufacturer.name} - {self.name}'
