1e72ca825c258d185f4c934739a17394248d1fa3
2siwon <lee89640245@gmail.com>
2018-04-08 19:08:31 +0900

    proxy table

--- test2/inheritance/admin.py
+++ test2/inheritance/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import Student, Teacher, School, Place, Restaurant
+from .models import Student, Teacher, School, Place, Restaurant, Supplier, Champion
+
+
+class ChampionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'champion_type', 'rank',)
+    list_editable = ('rank',)
+    ordering = ('rank',)
+
 
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(School)
 admin.site.register(Place)
 admin.site.register(Restaurant)
+admin.site.register(Supplier)
+admin.site.register(Champion, ChampionAdmin)
--- test2/inheritance/models/multi_table.py
+++ test2/inheritance/models/multi_table.py
@@ -3,6 +3,7 @@ from django.db import models
 __all__ = (
     'Place',
     'Restaurant',
+    'Supplier',
 )
 
 
@@ -10,7 +11,20 @@ class Place(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=80)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Restaurant(Place):
     serves_hot_dogs = models.BooleanField(default=False)
     serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} Restaurant'
+
+
+class Supplier(Place):
+    customer = models.ManyToManyField(
+        Place,
+        related_name='customer_place_set',
+    )
